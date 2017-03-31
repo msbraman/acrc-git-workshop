8795fab6f4f50cd51332654237d7f03966417e1c
Ram MSE <hariharaputran@ihpc.a-star.edu.sg>
2017-03-31 16:16:45 +0800

    new features

--- main.c
+++ main.c
@@ -8,4 +8,9 @@ printf("hello This is the original line\n");
 
 
 printf("hello1 This is the change we adding in next edition\n");
+
+printf("adding new features in the master branch\n");
+
+
+printf("checking the new feature");
 }
